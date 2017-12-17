81211b746b7c044d17bb0c5dc14b62fc88330c7f
taoxinyi <850804004@qq.com>
2017-12-17 22:53:43 +0800

    Slight modification

--- main.py
+++ main.py
@@ -238,7 +238,9 @@ class Ui_MainWindow(object):
   
         self.pro = QtWidgets.QLineEdit(self.centralwidget)
         self.pro.setFont(QFont('Consolas', 10, QFont.Light)) 
-        self.pro.setCompleter(QtWidgets.QCompleter(["ip","ip6","tcp","udp","arp","icmp","icmp6","igmp"]))
+        completer=QtWidgets.QCompleter(["ip","ip6","tcp","udp","arp","icmp","icmp6","igmp"])
+        completer.popup().setFont(QFont('Consolas', 10, QFont.Light))
+        self.pro.setCompleter(completer)
         self.pro.textChanged.connect(self.EvtTextPro)
     
 
